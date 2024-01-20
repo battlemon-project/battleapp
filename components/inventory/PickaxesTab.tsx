17fff52364f90bef22dc88320676813ca6fa712e
anbork <tonboruz@gmail.com>
2024-01-20 14:15:35 +0300

    Add mining scene to hub

--- components/inventory/PickaxesTab.tsx
+++ components/inventory/PickaxesTab.tsx
@@ -15,15 +15,15 @@ export default function PickaxesTab() {
 
   return (<div className="row">
     {size.width > 992 && <div className="col-5">
-      <div className="position-relative p-5">
+      <div className="position-relative">
         <img src='data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==' width='1000' height='1000' className='img-fluid' />
-        <div className={styles.generatorContainer}>
+        <div className={styles.miningContainer}>
           <PickaxeScene pickaxeType={ selectedPickaxe?.image.split('/').pop()?.split('.')[0] } />
         </div>
       </div>
     </div>}
 
-    <div className={cn('col-lg-7 col-12 position-relative', styles.inventoryContainer)}>
+    <div className={cn('col-lg-7 col-12 position-relative mx-0', styles.inventoryContainer)}>
       {/* {selectedPickaxe && <NftProps token={selectedPickaxe} />} */}
       {stage == 'Start' && <PickaxeStart balance={balance} />}
     </div>
--- components/inventory/scenes/PickaxeModel.tsx
+++ components/inventory/scenes/PickaxeModel.tsx
@@ -1,3 +1,5 @@
+import { AbstractMesh, AnimationGroup, Nullable, TransformNode, Vector3 } from '@babylonjs/core';
+import { useState } from 'react';
 import { ILoadedModel, Model, useScene } from 'react-babylonjs'
 
 interface PickaxeModelProps { 
@@ -5,19 +7,54 @@ interface PickaxeModelProps {
 }
 
 export default function BoxModel({ pickaxeType }: PickaxeModelProps) {
-  const baseUrl = (false && process.env.NEXT_PUBLIC_ASSETS || '') + '/models/boxes/';
+  const baseUrl = (false && process.env.NEXT_PUBLIC_ASSETS || '') + '/models/mining/';
+  const [ pickaxes, setPickaxes ] = useState<TransformNode[]>();
+  const [ placeholderGem, setPlaceholderGem] = useState<AbstractMesh>()
+  const [ miningAnimation, setMiningAnimation] = useState<Nullable<AnimationGroup>>()
+  const [ gemAppearAnimation, setGemAppearAnimation] = useState<Nullable<AnimationGroup>>()
+  const [ happyLemonAnimation, setHappyLemonAnimation] = useState<Nullable<AnimationGroup>>()
+  const [ sadLemonAnimation, setSadLemonAnimation] = useState<Nullable<AnimationGroup>>()
+  const [ sharpingAnimation, setSharpingAnimation] = useState<Nullable<AnimationGroup>>()
   const scene = useScene();
 
   const onPickaxeLoaded = (model: ILoadedModel): void => {
     if (!scene) return;
+    const idleAnimation = scene.getAnimationGroupByName('Idle');
+    idleAnimation?.start(true, 1);
+    const _pickaxes: TransformNode[] = [
+      scene.getTransformNodeByName('IcePick_Blue') as TransformNode,
+      scene.getTransformNodeByName('IcePick_Purple') as TransformNode,
+      scene.getTransformNodeByName('IcePick_Yellow') as TransformNode,
+    ]
+    _pickaxes.forEach(pa => {
+      pa.scaling = new Vector3(0, 0, 0);
+    })
+    setPickaxes(_pickaxes);
+    const _placeholderGem = scene.getMeshByName('placeholder_gem');
+    if (_placeholderGem)  {
+      _placeholderGem.visibility = 0;
+      setPlaceholderGem(_placeholderGem)
+    }
+    const _miningAnimation = scene.getAnimationGroupByName('Mining');
+    setMiningAnimation(_miningAnimation)
+    const _gemAppearAnimation = scene.getAnimationGroupByName('GemAppear');
+    setGemAppearAnimation(_gemAppearAnimation)
+    const _happyLemonAnimation = scene.getAnimationGroupByName('HappyLemon');
+    setHappyLemonAnimation(_happyLemonAnimation)
+    const _sadLemonAnimation = scene.getAnimationGroupByName('SadLemon');
+    setSadLemonAnimation(_sadLemonAnimation)
+    const _sharpingAnimation = scene.getAnimationGroupByName('Sharping');
+    _sharpingAnimation?.start(false, 1, 240, 241);
+    setSharpingAnimation(_sharpingAnimation)
   }
+
 
   return (
     <Model
-      id={'Lemon_Mining'}
-      name={'Lemon_Mining'}
+      id={'BTLMN_Lemon_Mining'}
+      name={'BTLMN_Lemon_Mining'}
       rootUrl={baseUrl}
-      sceneFilename={`Basket_Chests_LP_oneReward.gltf`}
+      sceneFilename={`BTLMN_Lemon_Mining.gltf`}
       scaleToDimension={undefined}
       onModelLoaded={onPickaxeLoaded}
     />
