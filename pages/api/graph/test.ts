501de459b898b2f91b02762b82cbba94971ee175
anbor <an@bor.com>
2024-02-14 14:25:13 +0200

    Update

--- pages/api/graph/test.ts
+++ pages/api/graph/test.ts
@@ -26,7 +26,6 @@ export default async function handler (req: NextRequest) {
               owner: {id: {equalTo: "${address}"}}
             }) {
               nodes {
-                id
                 tokenId
                 tokenUri
                 contract
