15335e54a1fbcbce4aa8829f62501042cef83609
anbork <tonboruz@gmail.com>
2024-03-03 12:52:56 +0300

    Test websockets

--- components/pages/shop/BuyBoxPage.tsx
+++ components/pages/shop/BuyBoxPage.tsx
@@ -1,105 +1,92 @@
 import cn from 'classnames';
 import styles from './shop.module.css'
-import Link from 'next/link';
 import BuyBox from './buttons/BuyBox';
-import useAuth from 'context/AuthContext';
-import { truncate } from 'utils/misc';
 import { BoxType, PrizeType, prizes } from 'hooks/useBuyBox';
-import { SignInButton } from './buttons/SignInButton';
 import BoxScene from './scenes/BoxScene';
 import { useEffect, useState } from 'react';
 import { useAccount, useNetwork } from 'wagmi';
 import { decodeEventLog, parseAbi } from 'viem';
 import { useBoxStore } from './store/boxStore';
+import useWebSocket from 'react-use-websocket';
 
 export default function BuyBoxPage() {
   const { address }  = useAccount();
   const { chain } = useNetwork();
   const { setPrize } = useBoxStore()
-  const { isSignedIn, isSupportedChain } = useAuth();
-
-  useEffect(() => {
-    if (!chain || !address) return;
-
-    const query = `
-      subscription {
-        eventUsers(
-          id:"${address}_${chain.id}_box",
-        ) {
-          id,
-          _entity
+  const { lastMessage, sendMessage } = useWebSocket(process.env.NEXT_PUBLIC_SUBQUERY_IP!, {
+    onOpen: () => {
+      const query = `
+        subscription {
+          eventUsers(
+            id:"${address}_${chain?.id}_box",
+          ) {
+            id,
+            _entity
+          }
         }
-      }
-    `
-    
-    const webSocket = new WebSocket(process.env.NEXT_PUBLIC_SUBQUERY_IP!, "graphql-ws");
-  
-    webSocket.onopen = event => {
-      webSocket.send(JSON.stringify({
+      `
+      sendMessage(JSON.stringify({
         type: 'start',
         id: 1,
         payload: { query }
       }))
-    }
+    },
+    //shouldReconnect: (closeEvent) => true,
+  });
 
-    webSocket.onmessage = event => {
-      const data = JSON.parse(event.data as string)
-      if (data.type !== 'data') return
-      const { data: logData, topics } = data.payload.data.eventUsers._entity
 
-      const decoded = decodeEventLog({
-        abi: parseAbi(['event Prize(bytes32, address, uint)']),
-        data: logData,
-        topics: topics
-      })
-      console.log(decoded)
-      const prize = prizes[Number(decoded.args[2])]
-      setPrize(prize)
-    }
+
+  // useEffect(() => {
+  //   if (!chain || !address) return;
+
+
     
-    return () => {
-      webSocket.close();
-    }
-  }, [chain, address])
+  //   const webSocket = new WebSocket(process.env.NEXT_PUBLIC_SUBQUERY_IP!, "graphql-ws");
+  
+  //   webSocket.onopen = event => {
+  //     webSocket.send(JSON.stringify({
+  //       type: 'start',
+  //       id: 1,
+  //       payload: { query }
+  //     }))
+  //   }
 
+  //   webSocket.onmessage = event => {
+  //     const data = JSON.parse(event.data as string)
+  //     if (data.type !== 'data') return
+  //     const { data: logData, topics } = data.payload.data.eventUsers._entity
+
+  //     const decoded = decodeEventLog({
+  //       abi: parseAbi(['event Prize(bytes32, address, uint)']),
+  //       data: logData,
+  //       topics: topics
+  //     })
+  //     console.log(decoded)
+  //     const prize = prizes[Number(decoded.args[2])]
+  //     setPrize(prize)
+  //   }
+    
+  //   return () => {
+  //     webSocket.close();
+  //   }
+  // }, [chain, address])
 
   return (<>
     <div className="container py-3 mb-auto">
-      <Link href="/shop">
-        <button className='btn btn-outline-light'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-left mb-05" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-          </svg>
-          <span className='ps-2'>Back to Shop</span>
-        </button>
-      </Link>
-      <br /><br /><br />
-      {(() => {
-        if (!isSignedIn || !isSupportedChain) {
-          return <div className='row justify-content-center'>
-            <div className='col-4'>
-              <SignInButton />
-            </div>
-          </div>
-        } else {
-          return <>
-            <div style={{height: '400px'}}>
-              <BoxScene name='Basket_Chests_LP_oneReward' debug={false} />
-            </div>
-            <div className='row'>
-              <div className='col-md-4 col-12'>
-                <BuyBox boxType={BoxType.Cheap} />
-              </div>
-              <div className='col-md-4 col-12'>
-                <BuyBox boxType={BoxType.Good} />
-              </div>
-              <div className='col-md-4 col-12'>
-                <BuyBox boxType={BoxType.Great} />
-              </div>
-            </div>
-          </>}
-      })()}
-
+      <div style={{height: '400px'}}>
+        <BoxScene name='Basket_Chests_LP_oneReward' debug={false} />
+      </div>
+      <div className='row'>
+        <div className='col-md-4 col-12'>
+          <BuyBox boxType={BoxType.Cheap} />
+        </div>
+        <div className='col-md-4 col-12'>
+          <BuyBox boxType={BoxType.Good} />
+        </div>
+        <div className='col-md-4 col-12'>
+          <BuyBox boxType={BoxType.Great} />
+        </div>
+      </div>
     </div>
   </>);
 };
--- pages/shop/box.tsx
+++ pages/shop/box.tsx
@@ -1,16 +1,36 @@
 import Layout from 'components/layout/Web3Layout';
 import BuyBoxPage from 'components/pages/shop/BuyBoxPage';
+import { SignInButton } from 'components/pages/shop/buttons/SignInButton';
 import BoxProvider from 'components/pages/shop/store/BoxProvider';
 import Head from 'next/head';
+import Link from 'next/link';
+import { useAccount, useNetwork } from 'wagmi';
 
 export default function Shop() {
+  const { address }  = useAccount();
+  const { chain } = useNetwork();
+  
   return (<>
     <Head>
       <title>Buy Chest - Battlemon</title>
     </Head>
     <Layout>
       <BoxProvider>
-        <BuyBoxPage />
+        <Link href="/shop">
+          <button className='btn btn-outline-light'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-left mb-05" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg>
+            <span className='ps-2'>Back to Shop</span>
+          </button>
+        </Link>
+        {address && chain ? <BuyBoxPage /> : <>
+          <div className='row justify-content-center'>
+            <div className='col-4'>
+              <SignInButton />
+            </div>
+          </div>
+        </>}
       </BoxProvider>
     </Layout>
   </>);
