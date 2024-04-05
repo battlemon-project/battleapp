3640f82bb9e9465c37a8f1f9120d5ea15d7ea2c2
anbor <an@bor.com>
2024-04-05 17:13:34 +0200

    Update pages

--- components/pages/GoldenKeyPage.tsx
+++ components/pages/GoldenKeyPage.tsx
@@ -10,6 +10,7 @@ import { useChainModal } from '@rainbow-me/rainbowkit';
 import { useContract } from 'hooks/useContract';
 import useAuth from 'context/AuthContext';
 import BuyGoldenKey from './shop/buttons/BuyGoldenKey';
+import Timer from 'components/layout/Timer';
 
 
 export default function GoldenKeyPage() {
@@ -26,13 +27,22 @@ export default function GoldenKeyPage() {
           <video autoPlay={true} muted preload="auto" loop className='w-100 mx-auto' style={{maxWidth: '400px'}}>
             <source src="/media/Keycard.webm" type="video/webm" />
           </video>
+          
+          <p className="mx-3 rounded-3 mb-4" style={{borderLeft: '5px solid rgba(255,255,255,0.4)', background: 'rgba(255,255,255,0.1)', padding: '8px 15px', fontSize: '15px'}}>
+            Golden single KEY open single BOX once per 24H;<br />
+            You are guaranteed to receive points in the BOX;<br />
+            The purchased KEY never breaks.
+          </p>
         </div>
         <div className="col-12 col-md-7">
           
           <div className={cn('p-3 py-4 rounded-4 mb-4', shopStyles.lightBg)}>
-            <p className="mb-1">Omnichain NFT with built-in Battlemon Points miner and the ability to open a special box once a day</p>
+            <p className="mb-1">Omnichain NFT with built-in Battlemon Points miner and the ability to open a special box BOX a day</p>
             <p className="mb-3">70% of the funds collected from the sale of “golden keys” will go to the LXP-L earning pool, most of which will be distributed among BP holders</p>
-            
+            <div className="d-flex justify-content-between mb-2">
+              <b>When the timer ends, all unsold NFTs will be burned</b>
+              <div style={{fontSize: '18px'}}><Timer deadline={1713433238000} onFinished={() => {}} isDays={true} /></div>
+            </div>
             <div className="d-flex justify-content-between mb-2">
               <b>Contract Address</b>
               <div>{truncate(NEXT_PUBLIC_CONTRACT_KEY, 8)}</div>
@@ -46,7 +56,6 @@ export default function GoldenKeyPage() {
               <div className='fs-16'><PolSymbol>{chain.name}</PolSymbol></div>
             </div>}
           </div>
-          
           <div className='order-md-2 mb-3'>
             {address ? <>
               {chain && chain?.id !== 137 ? <BuyGoldenKey chain={chain} contract={NEXT_PUBLIC_CONTRACT_KEY!} address={address} /> : <>
@@ -58,11 +67,21 @@ export default function GoldenKeyPage() {
                 <SignInButton />
             </>}
           </div>
-          <a href={`https://element.market/assets/linea/0x534F9515875B0FA3103ea78031217632Cac8c3D0`} className="d-flex rounded-3 btn btn-primary w-100 mt-3 py-2 align-items-center justify-content-center fs-15" target='_blank'>
-            <img src="https://element.market/resource/images/favicon-32.png" />
-            <span>&nbsp; Battlemon Golden Key on Element &nbsp; </span>
-          </a>
-          
+          <div className="row">
+            <div className="col-6">
+              <a href={`https://element.market/assets/linea/0x534F9515875B0FA3103ea78031217632Cac8c3D0`} className="d-flex rounded-3 btn btn-primary w-100 mt-3 py-2 align-items-center justify-content-center fs-18" target='_blank'>
+                <img src="https://element.market/resource/images/favicon-32.png" />
+                <span>&nbsp; Keys on Element</span>
+              </a>
+            </div>
+                
+            <div className="col-6">
+              <a href={`/game`} className="d-flex rounded-3 btn btn-default w-100 mt-3 py-2 align-items-center justify-content-center fs-18">
+                <img src="/images/shop/boxicon.png" height="29" className='rounded-4' />
+                <span>&nbsp; Open BOX</span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- components/pages/shop/buttons/BuyGoldenKey.tsx
+++ components/pages/shop/buttons/BuyGoldenKey.tsx
@@ -53,7 +53,7 @@ export default function BuyGoldenKey({ chain, address, contract}: BuyGoldenKeyPr
           </div>
         }
       </button>
-      <button className={cn('btn btn-lg btn-outline-light py-1', { disabled: count > 9 })} onClick={() => setCount(count + 1)}>
+      <button className={cn('btn btn-lg btn-outline-light py-1', { disabled: count > 19 })} onClick={() => setCount(count + 1)}>
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16"><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/></svg>
       </button>
     </div>
